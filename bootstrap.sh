68a4844e092888b72838b73386feb88d44827f6c
K Birt <AgentK9@users.noreply.github.com>
2024-02-15 11:13:38 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,12 @@
+# install dependencies TODO: determine package manager
+apt install stow zsh
+
+# set zsh as default
+chsh -s $(which zsh)
+
+# set up zsh
 WD="$HOME/.dotfiles"
 ln -s $WD/powerlevel10k $WD/.oh-my-zsh/custom/themes/
 
+# stow stuff
 stow .
